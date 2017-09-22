6d678fa19dc6752dcaba99f57f4feb4689015281
Oscar Galván <ramirrro1106@gmail.com>
2017-09-22 17:05:55 -0600

    Campos personalizados cliente
    Modificaciones al modulo de clientes, se añadieron campos.

--- models/crm_custom_fields.py
+++ models/crm_custom_fields.py
@@ -5,3 +5,6 @@ from odoo import api, fields, models
 
 class CrmCustomFields(models.Model):
   _inherit = "crm.lead"
+
+
+  
--- models/partner_custom_fields.py
+++ models/partner_custom_fields.py
@@ -6,9 +6,13 @@ from odoo import api, fields, models
 class PartnerCustomFields(models.Model):
   _inherit = "res.partner"
 
-  lb_razon_social       = fields.Char(string="Razón Social", help="Razón Social")
-  lb_domicilio_fiscal   = fields.Char(string="Domicilio Fiscal", help="Domicilio Fiscal")
-  lb_rfc                = fields.Char( string="RFC", help="RFC")
+  lbn_razon_social       = fields.Char(string="Razón Social", help="Razón Social")
+  lbn_domicilio_fiscal   = fields.Char(string="Domicilio Fiscal", help="Domicilio Fiscal")
+  lbn_rfc                = fields.Char(string="RFC", help="RFC")
+  lbn_nombre_cia		 = fields.Char(string="Nombre CIA", help="Nombre CIA")
+  lbn_id                 = fields.Char(string="I.D", help="I.D") 
+  lbn_cargo				 = fields.Char(string="Cargo", help="Cargo")
+  lbn_giro				 = fields.Char(string="Giro", help="Giro")
 
   
 
